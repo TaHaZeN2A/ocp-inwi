50c02e33f7c9db0dfcebcd664a339df7a842b730
TaHaZeN2A <tahazenjari2a@gmail.com>
2024-07-01 14:36:46 +0100

    3

---
+++ myapp/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,21 @@
+import { SignIn, ClerkLoaded, ClerkLoading} from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+export default function Page() {
+  return (
+  <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+    <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+        </div>
+        <ClerkLoaded>
+        <SignIn path="/sign-in" />
+        </ClerkLoaded>
+        <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground"/>
+        </ClerkLoading>
+    </div>
+    <div className="h-full bg-blue-200 hidden lg:flex items-center justify-center">
+        <h1>asdsad</h1>
+    </div>
+  </div>
+  );
+}
---
+++ myapp/middleware.ts
@@ -0,0 +1,19 @@
+import { auth, clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+export default clerkMiddleware((auth, request) => {
+    if (isProtectedRoute(request)){
+        auth().protect();
+    }
+    return NextResponse.next();
+})
+
+const isProtectedRoute = createRouteMatcher([
+    "/",
+  ]);
+
+export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
